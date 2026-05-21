bd91a2d39a2225454066fd4ae3b5b1a71af47424
ManoMonts <monterinbrabo@gmail.com>
2026-05-21 17:30:54 -0300

    fix: remove sample statement data

--- src/lib/financeRules.ts
+++ src/lib/financeRules.ts
@@ -110,13 +110,4 @@ export const incomeRules: CategoryRule[] = [
   { category: 'Rendimentos', keywords: ['rendimento', 'juros capital', 'investimento'] },
 ];
 
-export const sampleText = `01/05/2026;PIX RECEBIDO CLIENTE;2500,00
-02/05/2026;MERCADO MUFFATO;-286,43
-03/05/2026;UBER VIAGEM;-32,90
-05/05/2026;IFOOD RESTAURANTE;-48,70
-07/05/2026;POSTO COMBUSTIVEL;-180,00
-10/05/2026;NETFLIX;-39,90
-12/05/2026;FARMACIA DROGARIA;-67,35
-15/05/2026;SALARIO EMPRESA;3200,00
-16/05/2026;ALUGUEL;-950,00
-18/05/2026;AMAZON COMPRA;-129,90`;
+export const sampleText = '';
