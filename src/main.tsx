dc2cc2284a014be4a8e3cbbdb16b9d8d62094080
ManoMonts <monterinbrabo@gmail.com>
2026-05-22 16:13:31 -0300

    feat: load monthly history comparison panel

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './authBootstrap';
 import './authFlowBootstrap';
 import './incomeCompareBootstrap';
 import './savedAnalysesBootstrap';
+import './monthlyHistoryBootstrap';
 
 const rootElement = document.getElementById('root');
 
