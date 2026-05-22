9504dc3daab1d93460cad46b80e5766357105691
ManoMonts <monterinbrabo@gmail.com>
2026-05-22 12:04:18 -0300

    fix: replace replaceAll for TypeScript target compatibility

--- src/incomeCompareBootstrap.ts
+++ src/incomeCompareBootstrap.ts
@@ -48,11 +48,11 @@ function renderIncomePanel(panel: HTMLElement) {
 
 function escapeHtml(value: string) {
   return value
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#039;');
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
 }
 
 function ensureIncomeComparePanel() {
